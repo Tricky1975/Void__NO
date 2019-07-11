1db0fdd34a63552fa7fc1d764d10f3597f047ef4
Jeroen P. Broks <facebook@tbbs.nl>
2019-07-11 15:27:11 +0200

    Base Window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -47,6 +47,10 @@ namespace Void {
         public MainWindow() {
             InitializeComponent();
         }
+
+        private void TextChangedEventHandler(object sender, TextChangedEventArgs e) {
+
+        }
     }
 }
 
