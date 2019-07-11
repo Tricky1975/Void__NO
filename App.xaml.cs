b82d02497b627e45dd552fa5cac50ff9dff06125
Jeroen P. Broks <facebook@tbbs.nl>
2019-07-11 16:05:33 +0200

    Highlighter init call when loading application

--- App.xaml.cs
+++ App.xaml.cs
@@ -38,6 +38,10 @@ namespace Void {
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+
+        public App() {
+            Void.Highlights.Algemeen.Init();
+        }
     }
 }
 
