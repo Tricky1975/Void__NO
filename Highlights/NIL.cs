7168a93d0c9784159527ca6a2aff5dba6f4e67a2
Jeroen P. Broks <facebook@tbbs.nl>
2019-07-11 15:51:29 +0200

    Let's get up the stuff to do actual highlighting!

---
+++ Highlights/NIL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Void.Highlights {
+    class NIL:Algemeen {
+        public override void Highlight(RichTextBox RTB) {
+            throw new NotImplementedException();
+        }
+    }
+}
